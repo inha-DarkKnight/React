f98f0b5edb75225195d47d5bf23a8c750d802e05
hulhwa <eapp2002@gmail.com>
2023-11-20 20:24:26 +0900

    proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,18 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: '180.230.174.144:8000',
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    '/mongodb',
+    createProxyMiddleware({
+      target: 'http://localhost:27017',
+      changeOrigin: true,
+    })
+  );
+};
